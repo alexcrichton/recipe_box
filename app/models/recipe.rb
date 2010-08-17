0be9838c52c4df660d4c475779aa44f9a335d26c
Alex Crichton <alex@alexcrichton.com>
2010-08-17 02:54:05 -0500

    Rails.js + Jquery

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,5 +5,6 @@ class Recipe < ActiveRecord::Base
   validates_presence_of :name, :directions
   validates_uniqueness_of :name
   
-  accepts_nested_attributes_for :ingredients, :reject_if => :all_blank
+  accepts_nested_attributes_for :ingredients, :reject_if => :all_blank,
+      :allow_destroy => true
 end
