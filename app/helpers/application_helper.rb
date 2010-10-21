3708d081262f78654b67e7403e79bd0aee00d84f
Alex Crichton <alex@alexcrichton.com>
2010-10-21 16:07:52 -0400

    Remove unneeded code things

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,4 @@
 module ApplicationHelper
-  def fb_login_url
-    @login_url = MiniFB.oauth_url(
-        Rails.application.config.fb_app_id, login_url, :scope => 'email')    
-  end
-  
   def markdown text
     BlueCloth.new(text).to_html.html_safe
   end
