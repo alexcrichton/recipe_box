4ec763dc0412595b8a5c90718dd79e0a6cff893b
Alex Crichton <alex@alexcrichton.com>
2010-10-21 16:12:24 -0400

    Add a helper function to fetch all registered friends of the current user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def markdown text
     BlueCloth.new(text).to_html.html_safe
   end
+
+  def current_friends
+    ids = if current_user
+      Rails.cache.fetch(current_user.fb_uid + '_fb_friends') do
+        MiniFB.get(current_user.access_token, current_user.fb_uid,
+          :type => 'friends').data
+      end
+    else
+      []
+    end
+
+    User.where :id => ids.map(&:id)
+  end
 end
