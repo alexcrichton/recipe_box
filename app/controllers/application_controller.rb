786676e6a1fb719964150cf7b6f4d017317e371c
Alex Crichton <alex@alexcrichton.com>
2010-10-21 15:44:11 -0400

    Use the timezone attribute on users to set the current time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_user_time_zone
+
   rescue_from CanCan::AccessDenied do |exn|
     redirect_to '/auth/facebook'
   end
 
   helper :layout
+
+  protected
+
+  def set_user_time_zone
+    if current_user && current_user.timezone
+      Time.zone = ActiveSupport::TimeZone[current_user.timezone]
+    end
+  end
 end
