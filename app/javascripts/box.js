59d36676855b1a2df8cffbf7274f92c21a4584e6
Alex Crichton <alex@alexcrichton.com>
2010-10-30 18:49:13 -0400

    Track where the user is so a page refresh goes back to where they were

--- app/javascripts/box.js
+++ app/javascripts/box.js
@@ -4,9 +4,22 @@ $(function() {
   $('#buttons a').click(function() {
     $('#card').addClass('start').removeClass('end');
   });
-  
+
   $('#card a').click(function() {
     $('#card').removeClass('start').addClass('end');
+    window.location.hash = '';
     return false;
   });
+
+  $('a[data-remote]').live('ajax:before', function() {
+    window.location.hash = $(this).attr('href');
+  });
+
+  if (window.location.hash != '#' && window.location.hash != '') {
+    $('#buttons a:first').click();
+    $.ajax({
+      url: window.location.hash.substr(1),
+      dataType: 'script'
+    });
+  }
 });
