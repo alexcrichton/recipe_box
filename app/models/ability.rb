77174853410372449ac683d1883f2758803f2fbf
Alex Crichton <alex@alexcrichton.com>
2010-10-31 19:56:58 -0400

    Let everyone read the recipes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,12 @@ class Ability
   include CanCan::Ability
 
   def initialize user
+    # Facebook rakes pages liked, should let them read them.
+    can :read, Recipe
+
     return if user.nil?
 
-    can [:read, :comment], Recipe, :user_id => user.friends.map(&:id)
+    can :comment, Recipe, :user_id => user.friends.map(&:id)
     can :manage, Recipe, :user_id => user.id
     can :manage, Comment
   end
