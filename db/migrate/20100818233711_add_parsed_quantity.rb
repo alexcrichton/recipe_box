85d1aeb7da6fe06e9bfef9b7639025c8270904ef
Alex Crichton <alex@alexcrichton.com>
2010-11-02 23:58:26 -0400

    No schemas on MongoDB...

--- db/migrate/20100818233711_add_parsed_quantity.rb
+++
@@ -1,9 +0,0 @@
-class AddParsedQuantity < ActiveRecord::Migration
-  def self.up
-    add_column :ingredients, :parsed_quantity, :string
-  end
-
-  def self.down
-    remove_column :ingredients, :parsed_quantity
-  end
-end
--- db/migrate/20101030231612_add_counter_cache_to_recipes.rb
+++
@@ -1,9 +0,0 @@
-class AddCounterCacheToRecipes < ActiveRecord::Migration
-  def self.up
-    add_column :recipes, :comments_count, :integer
-  end
-
-  def self.down
-    remove_column :recipes, :comments_count
-  end
-end
